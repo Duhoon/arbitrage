403f071bac094838d5b8e01c2d29967f98f678ad
412ock <412ock@algorithm.capital>
2024-05-28 15:58:47 +0900

    edit: add Order DTO

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,7 +1,11 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
-export class Order {
-  @PrimaryGeneratedColumn()
-  id: number;
+export interface OrderHistoryDTO {
+  date: Date;
+  pair: string;
+  input: number;
+  cex_price: number;
+  dex_price: number;
+  cost: number;
+  cex_balance: number | string;
+  dex_balance: number | string;
+  memo?: string;
 }
