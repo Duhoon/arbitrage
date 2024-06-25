03f6fa50cb4a91ea3e5c2ff31d08861d6cf0e3ec
412ock <412ock@algorithm.capital>
2024-06-25 15:34:51 +0900

    feat: add Order test code

---
+++ src/core/order.service.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from '@nestjs/testing';
+import { CoreModule } from './core.module';
+import { OrderService } from './order.service';
+import { OperatorService } from './operator.service';
+import { AppModule } from 'src/app.module';
+import { Pair } from './pair';
+import { Pairs } from 'src/constants/pairs';
+
+describe('OrderService', () => {
+  const operatorService = {
+    pairs: [new Pair(Pairs[0])],
+  };
+  let orderService: OrderService;
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(OperatorService)
+      .useValue(operatorService)
+      .compile();
+
+    orderService = moduleRef.get<OrderService>(OrderService);
+  });
+
+  it('get asset balance', async () => {
+    const pairs = operatorService.pairs;
+    const pair = pairs[0];
+
+    const [token0Balance, token1Balance] = await orderService.getBalance(
+      pair.token0,
+      pair.token1,
+    );
+
+    console.log(
+      `token0Balance: ${token0Balance}, token1Balance: ${token1Balance}`,
+    );
+    expect(typeof token0Balance).toEqual('number');
+    expect(typeof token1Balance).toEqual('number');
+  });
+});
