c653a161a109a322c17e280f37e09cbf0acf2aa0
412ock <412ock@algorithm.capital>
2024-06-26 16:40:02 +0900

    edit: logger 디버깅 모드 추가

--- src/infra/logger/logger.service.ts
+++ src/infra/logger/logger.service.ts
@@ -1,21 +1,45 @@
 import { Injectable, LoggerService as ILoggerService } from '@nestjs/common';
-import { logger } from './winston.config';
+import { ConfigService } from '@nestjs/config';
+import * as winston from 'winston';
+import { Logger } from 'winston';
+
+const transports = [
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      winston.format.colorize({
+        all: true,
+      }),
+      winston.format.printf(({ timestamp, level, message, context, trace }) => {
+        return `${timestamp} [${context}] ${level}: ${message}${trace ? `\n${trace}` : ''}`;
+      }),
+    ),
+  }),
+];
 
 @Injectable()
 export class LoggerService implements ILoggerService {
+  logger: Logger;
+  constructor(private configService: ConfigService) {
+    this.logger = winston.createLogger({
+      level: this.configService.get('LOG_LEVEL') || 'info',
+      transports,
+    });
+  }
+
   log(message: any, context?: string) {
-    logger.info(message, { context });
+    this.logger.info(message, { context });
   }
 
   error(message: string, trace: string, context?: string) {
-    logger.error(message, { context, trace });
+    this.logger.error(message, { context, trace });
   }
 
   warn(message: string, context?: string) {
-    logger.error(message, { context });
+    this.logger.error(message, { context });
   }
 
   debug(message: string, context?: string) {
-    logger.debug(message, { context });
+    this.logger.debug(message, { context });
   }
 }
